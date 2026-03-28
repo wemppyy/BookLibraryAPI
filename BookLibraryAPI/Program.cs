4e47726f6eb8eca170f7a238a3c8786860384dd2
wemppyy <denisfrostaliuk@gmail.com>
2026-03-28 12:57:12 +0100

    Remove automatic DB migration on app startup
    Removed the code in Program.cs that applied database migrations
    automatically at application startup. Migrations must now be
    managed manually or through a separate process.

--- BookLibraryAPI/Program.cs
+++ BookLibraryAPI/Program.cs
@@ -74,12 +74,6 @@ namespace BookLibraryAPI
 
             var app = builder.Build();
 
-            using (var scope = app.Services.CreateScope())
-            {
-                var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
-                db.Database.Migrate();
-            }
-
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
